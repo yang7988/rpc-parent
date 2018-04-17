91e35dcbaaf8e4ef0b10125bec29ef0f13075750
xuyangyang <xuyangyang@rayvision.com>
2018-04-17 15:27:45 +0800

    更改zk地址

--- rpc-business/src/main/java/com/rayvision/rpc/business/config/ApplicationBeansConfig.java
+++ rpc-business/src/main/java/com/rayvision/rpc/business/config/ApplicationBeansConfig.java
@@ -4,50 +4,47 @@ import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.dubbo.config.ApplicationConfig;
 import com.alibaba.dubbo.config.ProtocolConfig;
 import com.alibaba.dubbo.config.RegistryConfig;
+
 import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ApplicationBeansConfig
-{
-  @Autowired
-  private DubboProperties dubboProperties;
-  
-  @Bean(name={"dataSource"}, destroyMethod="close", initMethod="init")
-  @ConfigurationProperties(prefix="spring.datasource")
-  public DataSource dataSource()
-  {
-    DruidDataSource druidDataSource = new DruidDataSource();
-    return druidDataSource;
-  }
-  
-  @Bean
-  public ApplicationConfig applicationConfig()
-  {
-    ApplicationConfig applicationConfig = new ApplicationConfig();
-    applicationConfig.setName(this.dubboProperties.getDubboApplicationName());
-    return applicationConfig;
-  }
-  
-  @Bean
-  public RegistryConfig registryConfig()
-  {
-    RegistryConfig registry = new RegistryConfig();
-    registry.setProtocol(this.dubboProperties.getDubboRegProtocol());
-    registry.setAddress(this.dubboProperties.getDubboRegAddress());
-    return registry;
-  }
-  
-  @Bean
-  public ProtocolConfig protocolConfig()
-  {
-    ProtocolConfig protocol = new ProtocolConfig();
-    protocol.setName(this.dubboProperties.getDubboProtocolName());
-    protocol.setPort(this.dubboProperties.getDubboProtocolPort());
-    protocol.setThreads(this.dubboProperties.getDubboProtocolThreads());
-    return protocol;
-  }
+public class ApplicationBeansConfig {
+    @Autowired
+    private DubboProperties dubboProperties;
+
+    @Bean(name = {"dataSource"}, destroyMethod = "close", initMethod = "init")
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        return druidDataSource;
+    }
+
+    @Bean
+    public ApplicationConfig applicationConfig() {
+        ApplicationConfig applicationConfig = new ApplicationConfig();
+        applicationConfig.setName(this.dubboProperties.getDubboApplicationName());
+        return applicationConfig;
+    }
+
+    @Bean
+    public RegistryConfig registryConfig() {
+        RegistryConfig registry = new RegistryConfig();
+        registry.setProtocol(this.dubboProperties.getDubboRegProtocol());
+        registry.setAddress(this.dubboProperties.getDubboRegAddress());
+        return registry;
+    }
+
+    @Bean
+    public ProtocolConfig protocolConfig() {
+        ProtocolConfig protocol = new ProtocolConfig();
+        protocol.setName(this.dubboProperties.getDubboProtocolName());
+        protocol.setPort(this.dubboProperties.getDubboProtocolPort());
+        protocol.setThreads(this.dubboProperties.getDubboProtocolThreads());
+        return protocol;
+    }
 }
--- rpc-web/src/main/java/com/rayvision/rpc/web/config/ApplicationBeansConfig.java
+++ rpc-web/src/main/java/com/rayvision/rpc/web/config/ApplicationBeansConfig.java
@@ -9,29 +9,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ApplicationBeansConfig
-{
-  private static Logger logger = LoggerFactory.getLogger(ApplicationBeansConfig.class);
-  @Autowired
-  private DubboProperties dubboProperties;
-  
-  @Bean
-  public ApplicationConfig applicationConfig()
-  {
-    logger.info("dubbo消费者应用名称: " + this.dubboProperties.getDubboApplicationName());
-    ApplicationConfig applicationConfig = new ApplicationConfig();
-    applicationConfig.setName(this.dubboProperties.getDubboApplicationName());
-    return applicationConfig;
-  }
-  
-  @Bean
-  public RegistryConfig registryConfig()
-  {
-    RegistryConfig registry = new RegistryConfig();
-    logger.info("rpc-web注册中心协议: " + this.dubboProperties.getDubboRegProtocol());
-    registry.setProtocol(this.dubboProperties.getDubboRegProtocol());
-    logger.info("rpc-web注册中心地址: " + this.dubboProperties.getDubboRegAddress());
-    registry.setAddress(this.dubboProperties.getDubboRegAddress());
-    return registry;
-  }
+public class ApplicationBeansConfig {
+    private static Logger logger = LoggerFactory.getLogger(ApplicationBeansConfig.class);
+    @Autowired
+    private DubboProperties dubboProperties;
+
+    @Bean
+    public ApplicationConfig applicationConfig() {
+        logger.info("dubbo消费者应用名称: " + this.dubboProperties.getDubboApplicationName());
+        ApplicationConfig applicationConfig = new ApplicationConfig();
+        applicationConfig.setName(this.dubboProperties.getDubboApplicationName());
+        return applicationConfig;
+    }
+
+    @Bean
+    public RegistryConfig registryConfig() {
+        RegistryConfig registry = new RegistryConfig();
+        logger.info("rpc-web注册中心协议: " + this.dubboProperties.getDubboRegProtocol());
+        registry.setProtocol(this.dubboProperties.getDubboRegProtocol());
+        logger.info("rpc-web注册中心地址: " + this.dubboProperties.getDubboRegAddress());
+        registry.setAddress(this.dubboProperties.getDubboRegAddress());
+        return registry;
+    }
 }
