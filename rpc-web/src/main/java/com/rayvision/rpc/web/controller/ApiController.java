f968d9077c01c8449cec09765f2ace2a293fa4a9
xuyangyang <xuyangyang@rayvision.com>
2019-02-20 14:04:40 +0800

    rocket mq

--- rpc-web/src/main/java/com/rayvision/rpc/web/controller/ApiController.java
+++ rpc-web/src/main/java/com/rayvision/rpc/web/controller/ApiController.java
@@ -2,22 +2,31 @@ package com.rayvision.rpc.web.controller;
 
 import com.rayvision.rpc.common.ApiResponse;
 import com.rayvision.rpc.web.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/user")
 public class ApiController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApiController.class);
+
     @Autowired
     private UserService userService;
 
     @GetMapping("/purchase/product/{userId}/{payMethod}/{productId}/{quantity}")
-    public ApiResponse purchaseProduct(@PathVariable Integer userId, @PathVariable Integer productId,
+    public ApiResponse purchase(@PathVariable Integer userId, @PathVariable Integer productId,
                                        @PathVariable Integer quantity, @PathVariable Byte payMethod) {
-        return userService.purchaseProduct(userId, productId, quantity, payMethod);
+        return userService.purchase(userId, productId, quantity, payMethod);
+    }
+
+    @PostMapping(value = "/taskNotify")
+    public ApiResponse taskNotify(@RequestBody Map<String,Object> params) {
+        LOGGER.warn("======回调通知数据======="+params);
+        return ApiResponse.returnSuccess();
     }
 }
