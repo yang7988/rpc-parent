1eead14b37f9ae9a4e3518e627900b92c8d2252e
xuyangyang <xuyangyang@rayvision.com>
2019-02-25 11:02:32 +0800

    rocket mq

--- rpc-web/src/main/java/com/rayvision/rpc/web/controller/ApiController.java
+++ rpc-web/src/main/java/com/rayvision/rpc/web/controller/ApiController.java
@@ -2,11 +2,13 @@ package com.rayvision.rpc.web.controller;
 
 import com.rayvision.rpc.common.ApiResponse;
 import com.rayvision.rpc.web.service.UserService;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Enumeration;
 import java.util.Map;
 
 @RestController
@@ -25,8 +27,13 @@ public class ApiController {
     }
 
     @PostMapping(value = "/taskNotify")
-    public ApiResponse taskNotify(@RequestBody Map<String,Object> params) {
-        LOGGER.warn("======回调通知数据======="+params);
+    public ApiResponse taskNotify(@RequestBody Map<String,Object> params, HttpServletRequest request) {
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            LOGGER.warn(name + " = " + request.getHeader(name));
+        }
+        LOGGER.warn("======回调通知数据=======" + params);
         return ApiResponse.returnSuccess();
     }
 }
